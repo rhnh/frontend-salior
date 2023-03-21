31c459966d5cf3abc494daacbc9ea5a059cc2310
rhnh <rhnh@protonmail.com>
2023-03-21 11:29:51 +0100

    updating posts

---
+++ app/components/Posts.tsx
@@ -0,0 +1,12 @@
+import type { Post } from "@prisma/client";
+import DisplayPost from "./DisplayPost";
+
+export default function Posts({ posts }: { posts: Post[] }) {
+  return (
+    <article className="cards">
+      {posts.map((post) => (
+        <DisplayPost key={post.id} post={post} isTrim={true} />
+      ))}
+    </article>
+  );
+}
--- app/routes/posts/new.tsx
+++ app/routes/posts/new.tsx
@@ -3,16 +3,22 @@ import { createPost } from "~/models/post.server";
 import { redirect } from "@remix-run/node";
 import { getLocalAuthenticatedUser } from "~/utils/user.server";
 import { isAuthorizedUser } from "utils/user.server";
+import invariant from "tiny-invariant";
 
 export const action: ActionFunction = async ({ request }) => {
   const form = await request.formData();
   const title: string = form.get("title")?.toString() || "";
   const body: string = form.get("body")?.toString() || "";
+
+  const image: string =
+    form.get("image")?.toString() || "/assets/placeholder.jpg";
+  invariant(title, "No title provided");
+
   const user = await getLocalAuthenticatedUser(request);
   const username = user?.username;
   if (!username) return redirect("/users/login");
   if (isAuthorizedUser(user.role)) {
-    await createPost({ username, title, body });
+    await createPost({ username, title, body, image });
     return redirect("/posts");
   }
   return redirect("users/login");
@@ -20,15 +26,19 @@ export const action: ActionFunction = async ({ request }) => {
 
 export default function WriteNewArticle() {
   return (
-    <article>
-      <form method="post">
+    <article className="new-post" role="form">
+      <form method="post" aria-label="Write a new article">
         <p>
           <label htmlFor="title">Title</label>
           <input type="text" id="title" name="title" />
         </p>
         <p>
           <label htmlFor="body">Body</label>
-          <textarea id="body" name="body" />
+          <textarea id="body" name="body" rows={20} cols={30} />
+        </p>
+        <p>
+          <label htmlFor="image">Image</label>
+          <input type="text" id="image" name="image" />
         </p>
         <p>
           <button type="submit">Post</button>
