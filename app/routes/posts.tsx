cd5e00a1ef613d131e25dcf20eac7f02e45563d0
rhnh <rhnh@protonmail.com>
2023-03-24 15:02:27 +0100

    add new post

--- app/routes/posts.tsx
+++ app/routes/posts.tsx
@@ -1,31 +1,28 @@
-// import { json, redirect } from "@remix-run/node";
-// import { Link, Outlet, useLoaderData } from "@remix-run/react";
-// import type { LoaderFunction } from "@remix-run/node";
-// import { getLocalAuthenticatedUser, isAuthorizedUser } from "utils/user.server";
+import { json, redirect } from "@remix-run/node"
+import { Link, Outlet, useLoaderData } from "@remix-run/react"
+import type { LoaderFunction } from "@remix-run/node"
+import { getLocalAuthenticatedUser, isAuthorizedUser } from "utils/user.server"
 /**
  *
  * @param param0
  * @returns
  */
-/*
-export const loader: LoaderFunction = async ({ request }) => {
-  const authorizedUser = await getLocalAuthenticatedUser(request);
-  if (!authorizedUser) {
-    return redirect("/users/login");
-  }
-  const isAuthorized = isAuthorizedUser(authorizedUser?.role);
-  return json({ isAuthorized });
-};
-*/
 
-import { Outlet } from "@remix-run/react";
+export const loader: LoaderFunction = async ({ request }) => {
+  const authorizedUser = await getLocalAuthenticatedUser(request)
+  if (!authorizedUser) {
+    return redirect("/users/login")
+  }
+  const isAuthorized = isAuthorizedUser(authorizedUser?.role)
+  return json({ isAuthorized })
+}
 
 export default function Posts() {
-  // const { isAuthorized } = useLoaderData();
+  const { isAuthorized } = useLoaderData()
   return (
     <>
-      {/* {isAuthorized === true ? <Link to="new">Write a new article</Link> : null} */}
+      {isAuthorized === true ? <Link to="new">Write a new article</Link> : null}
       <Outlet />
     </>
-  );
+  )
 }
