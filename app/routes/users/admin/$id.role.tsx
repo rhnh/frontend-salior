6483b3c045060ce69542fefd1cc321b3b8a1b9d6
rhnh <rhnh@protonmail.com>
2023-04-11 14:08:34 +0200

    improved css allUsers

--- app/routes/users/admin/$id.role.tsx
+++ app/routes/users/admin/$id.role.tsx
@@ -1,86 +1,86 @@
-import { redirect } from "@remix-run/node";
-import { destroySession, getSession } from "~/utils/session.server";
-import type { ActionFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node"
+import { destroySession, getSession } from "~/utils/session.server"
+import type { ActionFunction } from "@remix-run/node"
 import {
   getLocalAuthenticatedUser,
   isAuthorizedUser,
-} from "~/utils/user.server";
-import type { LoaderFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { getUserById, setRole } from "~/models/user.server";
-import invariant from "tiny-invariant";
-import type { Role } from "@prisma/client";
+} from "~/utils/user.server"
+import type { LoaderFunction } from "@remix-run/node"
+import { json } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
+import { getUserById, setRole } from "~/models/user.server"
+import invariant from "tiny-invariant"
+import type { Role } from "@prisma/client"
 
 export const loader: LoaderFunction = async ({ request, params }) => {
-  const authorizedUser = await getLocalAuthenticatedUser(request);
+  const authorizedUser = await getLocalAuthenticatedUser(request)
   //if not logged in
-  const isAuthorized = isAuthorizedUser(authorizedUser?.role || "user");
+  const isAuthorized = isAuthorizedUser(authorizedUser?.role || "user")
   if (!authorizedUser) {
-    const session = await getSession(request.headers.get("Cookie"));
+    const session = await getSession(request.headers.get("Cookie"))
     if (typeof session === "object") {
       return redirect("/users/login", {
         headers: {
           "Set-Cookie": await destroySession(session),
         },
-      });
+      })
     }
   }
   //force logout
   if (isAuthorized === false) {
-    const session = await getSession(request.headers.get("Cookie"));
+    const session = await getSession(request.headers.get("Cookie"))
     if (typeof session === "object") {
       return redirect("/users/login", {
         headers: {
           "Set-Cookie": await destroySession(session),
         },
-      });
+      })
     }
   }
 
-  const id = params?.id;
+  const id = params?.id
   if (!id) {
-    throw new Response(`Invalid`, { status: 404 });
+    throw new Response(`Invalid`, { status: 404 })
   }
-  const user = await getUserById(id);
-  return json({ user });
-};
+  const user = await getUserById(id)
+  return json({ user })
+}
 export const action: ActionFunction = async ({ request, params }) => {
   // check the user id
-  const authorizedUser = await getLocalAuthenticatedUser(request);
+  const authorizedUser = await getLocalAuthenticatedUser(request)
   //if not logged in
-  const isAuthorized = isAuthorizedUser(authorizedUser?.role || "user");
+  const isAuthorized = isAuthorizedUser(authorizedUser?.role || "user")
   if (!authorizedUser) {
-    const session = await getSession(request.headers.get("Cookie"));
+    const session = await getSession(request.headers.get("Cookie"))
     if (typeof session === "object") {
       return redirect("/users/login", {
         headers: {
           "Set-Cookie": await destroySession(session),
         },
-      });
+      })
     }
   }
   //force logout
   if (isAuthorized === false) {
-    const session = await getSession(request.headers.get("Cookie"));
+    const session = await getSession(request.headers.get("Cookie"))
     if (typeof session === "object") {
       return redirect("/users/login", {
         headers: {
           "Set-Cookie": await destroySession(session),
         },
-      });
+      })
     }
   }
-  const form = await request.formData();
-  const id = params.id;
-  const role = form.get("role") as unknown as Role;
-  invariant(id, "invalid id");
-  invariant(role, "Invalid role");
-  await setRole({ role, id });
-  return redirect("/users/admin/users");
-};
+  const form = await request.formData()
+  const id = params.id
+  const role = form.get("role") as unknown as Role
+  invariant(id, "invalid id")
+  invariant(role, "Invalid role")
+  await setRole({ role, id })
+  return redirect("/users/admin/users")
+}
 export default function ChangeRole() {
-  const { user } = useLoaderData();
+  const { user } = useLoaderData()
   return (
     <article>
       <form method="post">
@@ -98,8 +98,8 @@ export default function ChangeRole() {
             <option value="admin">Administrator</option>
           </optgroup>
         </select>
-        <button type="submit">Change the role</button>
+        <button type="submit">Change the role </button>
       </form>
     </article>
-  );
+  )
 }
